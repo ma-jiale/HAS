02a0981192decc2503e14f9c266763bf6a59bc0d
ma-jiale <majialea@126.com>
2024-12-09 10:15:14 +0800

    增加窗帘的舵机数量

--- Software/control_and_recieve_data/control_and_recieve_data.ino
+++ Software/control_and_recieve_data/control_and_recieve_data.ino
@@ -6,7 +6,8 @@
 #define DHTTYPE DHT11   // 定义使用的DHT传感器类型
 #define LED_PIN 12
 #define MOTOR_PIN 5
-#define CURTAIN_PIN 3
+#define SERVO_PIN1 3
+#define SERVO_PIN2 6
 
 class Light {
   private:
@@ -82,21 +83,26 @@ class Fan {
 
 class SmartCurtain {
   private:
-    Servo curtainServo;  // 舵机对象
-    int pin;             // 舵机连接的引脚
+    Servo curtainServo1; 
+    Servo curtainServo2; // 舵机对象
+    int pin1;
+    int pin2;            // 舵机连接的引脚
     bool isOpen;         // 窗帘的当前状态，true 表示窗帘已打开，false 表示窗帘已关闭
 
   public:
     // 构造函数，初始化舵机并设置引脚，默认窗帘是关闭的
-    SmartCurtain(int servoPin) {
-      pin = servoPin;
+    SmartCurtain(int servoPin1, int servoPin2) {
+      pin1 = servoPin1;
+      pin2 = servoPin2;
       isOpen = false;  // 默认窗帘是关闭的
     }
 
     void begin()
     {
-      curtainServo.attach(pin);  // 将舵机连接到指定的引脚
-      curtainServo.write(0);     // 初始化时设置窗帘为关闭状态
+      curtainServo1.attach(pin1);  // 将舵机连接到指定的引脚
+      curtainServo1.write(0);
+      curtainServo2.attach(pin2);  // 将舵机连接到指定的引脚
+      curtainServo2.write(0);     // 初始化时设置窗帘为关闭状态
     }
 
     // 切换窗帘的开关状态
@@ -115,7 +121,8 @@ class SmartCurtain {
     // 开启窗帘，从0度转到180度
     void openCurtain() {
       for (int pos = 0; pos <= 180; pos++) {
-        curtainServo.write(pos);  // 设置舵机的位置
+        curtainServo1.write(pos);  // 设置舵机的位置
+        curtainServo2.write(pos);
         delay(15);  // 等待舵机移动到指定位置
       }
       isOpen = true;  // 更新窗帘状态为已打开
@@ -124,7 +131,8 @@ class SmartCurtain {
     // 关闭窗帘，从180度转到0度
     void closeCurtain() {
       for (int pos = 180; pos >= 0; pos--) {
-        curtainServo.write(pos);  // 设置舵机的位置
+        curtainServo1.write(pos);  // 设置舵机的位置
+        curtainServo2.write(pos); // 设置舵机的位置
         delay(15);  // 等待舵机移动到指定位置
       }
       isOpen = false;  // 更新窗帘状态为已关闭
@@ -139,7 +147,7 @@ class SmartCurtain {
 DHT dht(DHTPIN, DHTTYPE);
 Light myLight(LED_PIN);
 Fan myFan(MOTOR_PIN);
-SmartCurtain myCurtian(CURTAIN_PIN);
+SmartCurtain myCurtian(SERVO_PIN1, SERVO_PIN2);
 int pos = 0;
 int temperature, humidity;
 
